cc633c5a89379d33f9734e13ec978c714a4aa97a
Anish Sharma <aneeshsharma15@outlook.com>
2020-11-27 14:29:55 +0530

    implemented fork system call

---
+++ history/stage20/compile.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+STAGE=20
+
+DIR=../history/stage$STAGE
+
+cd ~/code/MyExpOS/
+
+echo "This script will build and prepare the XSM machine"
+echo
+echo "Starting build..."
+
+cd spl
+
+echo "Compiling kernel..."
+./spl $DIR/os_startup.spl
+./spl $DIR/boot_module.spl
+./spl $DIR/timer_int.spl
+./spl $DIR/exhandler.spl
+./spl $DIR/int7.spl
+./spl $DIR/scheduler_module.spl
+./spl $DIR/device_manager.spl
+./spl $DIR/resource_manager.spl
+./spl $DIR/console_interrupt_handler.spl
+./spl $DIR/int6.spl
+./spl $DIR/int9.spl
+./spl $DIR/process_manager.spl
+./spl $DIR/memory_manager.spl
+./spl $DIR/disk_int.spl
+./spl $DIR/int8.spl
+./spl $DIR/int10.spl
+
+echo "Kernel build completed!"
+
+cd ../expl
+
+echo "Compiling user programs..."
+
+./expl $DIR/idle.expl
+./expl $DIR/even.expl
+./expl $DIR/odd.expl
+./expl $DIR/prime.expl
+./expl $DIR/read.expl
+./expl $DIR/gcd.expl
+./expl $DIR/init.expl
+./expl $DIR/exc_test.expl
+./expl $DIR/test.expl
+./expl $DIR/bubble.expl
+./expl $DIR/list.expl
+./expl $DIR/exp.expl
+./expl $DIR/create.expl
+./expl $DIR/two_list.expl
+./expl $DIR/two_p.expl
+
+echo "User programs built!"
+
+echo "Loading kernel to disk..."
+
+cd ../xfs-interface
+
+./xfs-interface load --os $DIR/os_startup.xsm
+./xfs-interface load --library ../expl/library.lib
+./xfs-interface load --exhandler $DIR/exhandler.xsm
+./xfs-interface load --module 7 $DIR/boot_module.xsm
+./xfs-interface load --module 5 $DIR/scheduler_module.xsm
+./xfs-interface load --module 4 $DIR/device_manager.xsm
+./xfs-interface load --module 2 $DIR/memory_manager.xsm
+./xfs-interface load --module 1 $DIR/process_manager.xsm
+./xfs-interface load --module 0 $DIR/resource_manager.xsm
+./xfs-interface load --int=timer $DIR/timer_int.xsm
+./xfs-interface load --int=console $DIR/console_interrupt_handler.xsm
+./xfs-interface load --int=disk $DIR/disk_int.xsm
+./xfs-interface load --int=7 $DIR/int7.xsm
+./xfs-interface load --int=6 $DIR/int6.xsm
+./xfs-interface load --int=9 $DIR/int9.xsm
+./xfs-interface load --int=8 $DIR/int8.xsm
+./xfs-interface load --int=10 $DIR/int10.xsm
+
+echo "Loading user programs to disk..."
+
+./xfs-interface load --idle $DIR/idle.xsm
+./xfs-interface load --init $DIR/init.xsm
+./xfs-interface rm odd.xsm
+./xfs-interface rm prime.xsm
+./xfs-interface rm even.xsm
+./xfs-interface rm gcd.xsm
+./xfs-interface rm exc_test.xsm
+./xfs-interface rm test.xsm
+./xfs-interface rm bubble.xsm
+./xfs-interface rm list.xsm
+./xfs-interface rm exp.xsm
+./xfs-interface rm create.xsm
+./xfs-interface rm two_list.xsm
+./xfs-interface rm two_p.xsm
+./xfs-interface load --exec $DIR/odd.xsm
+./xfs-interface load --exec $DIR/prime.xsm
+./xfs-interface load --exec $DIR/even.xsm
+./xfs-interface load --exec $DIR/gcd.xsm
+./xfs-interface load --exec $DIR/exc_test.xsm
+./xfs-interface load --exec $DIR/test.xsm
+./xfs-interface load --exec $DIR/bubble.xsm
+./xfs-interface load --exec $DIR/list.xsm
+./xfs-interface load --exec $DIR/exp.xsm
+./xfs-interface load --exec $DIR/create.xsm
+./xfs-interface load --exec $DIR/two_list.xsm
+./xfs-interface load --exec $DIR/two_p.xsm
+
+echo "Disk load complete!"
+echo
+echo "Run the XSM machine to test"
